a3b6089f6bd08388f662cc5aa0dbe8ccb5c3b988
Zach Varga <me@varga.design>
2022-07-23 22:27:11 -0600

    This has been such a fun project! Thank you very much.
    Some things I did:
    
    - Fix parcel path in package.json, update "main" to app.js.
    - Add date() function to utils.js to return MM/DD/YYYY
    - Add nested API requests to get weather data
    - TODO: finish working through forEach loop to display corresponding icons and days.

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,17 @@
 export function convertDate(time) {
   return time * 1000;
 }
+
+/**
+ *
+ * Date - Return the current date in MM/DD/YYYY format
+ */
+export function date() {
+  const date = new Date(Date.now());
+  return date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+}
+
+/*
+ *
+ * Display Inner HTML for Weather App content
+ */
