d68ffdb39ca2fc818b7d50ef6a4ae358aec61ba6
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2022-09-21 12:47:12 +0100

    Added function to clear the form when a new wheather is requested
    To clear out the previious wheather section

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ console.log("hello world");
 const input = document.getElementById("zipcode-input");
 const form = document.getElementById("form");
 
+function clearForm() {
+	if (document.getElementsByClassName("weather-container")[0]) {
+		document.getElementsByClassName("weather-container")[0].remove();
+	}
+}
+
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	getDisplayWheather();
+	clearForm();
+	input.value = "";
 });
