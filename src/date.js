69e7d3a62c98789900df2dfaf142fb258649bcb8
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2022-09-21 12:32:16 +0100

    Added Required Icons for Wheather Description

---
+++ src/date.js
@@ -0,0 +1,11 @@
+const cloud = require("../img/cloudy.png");
+const rain = require("../img/rain.png");
+const snow = require("../img/snow.png");
+const sun = require("../img/sunny.png");
+
+const icons = {
+	cloud,
+	rain,
+	snow,
+	sun,
+};
