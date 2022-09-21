080c0ac0c73989eec92fa4fe938b4d926c6e7119
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2022-09-21 12:40:17 +0100

    Added Event listener for the form input

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 import { convertDate } from "./utils";
-
+import { getDisplayWheather } from "./utils";
 console.log("hello world");
+
+const input = document.getElementById("zipcode-input");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	getDisplayWheather();
+});
